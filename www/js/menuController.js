2989f48aee73028c307d6ff99bade9a6b733c21e
Diệp Sang <diepsang@Dieps-iMac.local>
2016-05-23 23:21:12 +0700

    Design template

---
+++ www/js/menuController.js
@@ -0,0 +1,16 @@
+/**
+*  Module
+*
+* Description
+*/
+angular.module('fashionApplication')
+.controller('menuController', ['$scope','STORE_URL','$firebaseArray',function($scope,STORE_URL,$firebaseArray) {
+  var ref = new Firebase(STORE_URL + "menu");
+  $scope.group = $firebaseArray(ref);
+  $scope.toggleGroup = function(group) {
+    group.show = !group.show;
+  };
+  $scope.isGroupShown = function(group) {
+    return group.show;
+  };
+}]);
